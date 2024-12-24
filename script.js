bca54e6304da8165510566f31f692da347fc8ad5
aryansighkumaya <98814995+aryansighkumaya@users.noreply.github.com>
2024-12-24 16:18:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    return `Hello, my name is ${this.name}, I am ${this.age} years old.`;
 };
 
 // Define the Employee class
@@ -18,7 +18,7 @@ Employee.prototype = Object.create(Person.prototype); // Inherit from Person
 Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`;
 };
 
 // Do not change code below this line
