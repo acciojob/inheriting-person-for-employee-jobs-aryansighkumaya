65a496c8ed006e6ce53344ec5467aebe81893574
aryansighkumaya <98814995+aryansighkumaya@users.noreply.github.com>
2024-12-24 16:16:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-// complete this js code
-function Person(name, age) {}
+// Define the Person class
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+Person.prototype.greet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
+
+// Define the Employee class
+function Employee(name, age, jobTitle) {
+    Person.call(this, name, age); // Call the Person constructor
+    this.jobTitle = jobTitle;
+}
+
+Employee.prototype = Object.create(Person.prototype); // Inherit from Person
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
 
 // Do not change code below this line
 window.Person = Person;
