e26c86a8acad9bf281bd51096a30c18ddec9b0b8
aryansighkumaya <98814995+aryansighkumaya@users.noreply.github.com>
2024-12-24 23:18:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-// Define the Person class
-function Person(name, age) {
-    this.name = name;
-    this.age = age;
+function Person(name,age){
+this.name=name
+this.age=age
+
+}
+Person.prototype.greet=function(){
+console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
+
+}
+function Employee(name,age,jobTitle){
+Person.call(this,name,age)
+this.jobTitle=jobTitle
+
 }
 
-Person.prototype.greet = function() {
-    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
-};
 
-// Define the Employee class
-function Employee(name, age, jobTitle) {
-    Person.call(this, name, age); // Call the Person constructor
-    this.jobTitle = jobTitle;
+Employee.prototype=Object.create(Person.prototype)
+Employee.prototype.constructor=Employee
+
+Employee.prototype.jobGreet=function(){
+console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+
 }
 
-Employee.prototype = Object.create(Person.prototype); // Inherit from Person
-Employee.prototype.constructor = Employee;
-
-Employee.prototype.jobGreet = function() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-};
-
-// Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
+
+
+
+
+
