4a709a8e8c9b9ddd71fd5c83156b97fbd3cf6c6a
aryansighkumaya <98814995+aryansighkumaya@users.noreply.github.com>
2024-12-24 22:34:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
 }
 function Employee(name,age,job){
 Person.call(name,age,this)
-
+this.job=job
 
 
 
@@ -27,8 +27,3 @@ console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my 
 }
 
 
-
-
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
